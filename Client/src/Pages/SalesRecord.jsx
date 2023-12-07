d7f4b4009978b01383b1c1ccad42cbbde50ed52a
SadmanJawad <jawadnabil977@gmail.com>
2023-12-07 13:32:03 +0600

    sales record tab added

--- Client/src/Pages/SalesRecord.jsx
+++ Client/src/Pages/SalesRecord.jsx
@@ -1,39 +1,88 @@
+import Container from "../shared/Container";
+
 const SalesRecord = () => {
   return (
-    <div>
-      <div role="tablist" className="tabs tabs-bordered">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "30vh",
+      }}
+    >
+      <div role="tablist" className="tabs tabs-lg tabs-bordered">
+        {/* //TODO Invoice */}
         <input
           type="radio"
           name="my_tabs_1"
           role="tab"
           className="tab"
-          aria-label="Tab 1"
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          Tab content 1
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_1"
-          role="tab"
-          className="tab"
-          aria-label="Tab 2"
+          aria-label="Invoice"
           checked
         />
         <div role="tabpanel" className="tab-content p-10">
-          Tab content 2
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Invoice</th>
+                  <th>Name</th>
+                  <th>Mobile</th>
+                  <th>Number of product</th>
+                  <th>Product Name</th>
+                  <th>Total Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>1</th>
+                  <td>Cy Ganderton</td>
+                  <td>Quality Control Specialist</td>
+                  <td>Littel, Schaden and Vandervort</td>
+                  <td>Canada</td>
+                  <td>12/16/2020</td>
+                  <td>Blue</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-
+        {/* //TODO Quotation */}
         <input
           type="radio"
           name="my_tabs_1"
           role="tab"
           className="tab"
-          aria-label="Tab 3"
+          aria-label="Quotation"
         />
         <div role="tabpanel" className="tab-content p-10">
-          Tab content 3
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Quotation</th>
+                  <th>Name</th>
+                  <th>Mobile</th>
+                  <th>Number of product</th>
+                  <th>Product Name</th>
+                  <th>Total Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>1</th>
+                  <td>Cy Ganderton</td>
+                  <td>Quality Control Specialist</td>
+                  <td>Littel, Schaden and Vandervort</td>
+                  <td>Canada</td>
+                  <td>12/16/2020</td>
+                  <td>Blue</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
